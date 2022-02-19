ac74fa98c94b3c58605e2ad1960192282c3c6ba9
th3james <th3james@katsuragi.local>
2022-02-19 11:31:45 +0000

    Add registers

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ fn main() {
     let _opcode: u16;
     // CHIP-8 has 4k memory
     let _memory: [u8; 4096];
+    let _v_registers: [u8; 16];
+    let _index_register: u16;
+    let _program_counter: u16;
 
     println!("Hello, world!");
 }
