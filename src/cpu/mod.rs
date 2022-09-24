30dd989928693142ff08b142e5944c7355dd8786
th3james <th3james@fastmail.fm>
2022-09-24 14:58:30 +0100

    Move memory sizes to constants, add overflow test

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -3,11 +3,15 @@ use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 
+// CHIP-8 has 4k memory
+const CHIP_8_MEMORY_SIZE: usize = 4096;
+const APPLICATION_START_ADDRESS: usize = 0x200;
+const MAX_GAME_SIZE: usize = (CHIP_8_MEMORY_SIZE - APPLICATION_START_ADDRESS) as usize;
+
 pub struct CPU {
     // opcodes are two bytes
     opcode: u16,
-    // CHIP-8 has 4k memory
-    memory: [u8; 4096],
+    memory: [u8; CHIP_8_MEMORY_SIZE],
 
     v_registers: [u8; 16],
     index_register: u16,
@@ -31,10 +35,10 @@ impl CPU {
     pub fn initialize() -> CPU {
         CPU {
             opcode: 0,
-            memory: [0; 4096],
+            memory: [0; CHIP_8_MEMORY_SIZE as usize],
             v_registers: [0; 16],
             index_register: 0,
-            program_counter: 0x200,
+            program_counter: APPLICATION_START_ADDRESS as u16,
             frame_buffer: [false; 64 * 32],
             delay_timer: 0,
             sound_timer: 0,
@@ -44,11 +48,15 @@ impl CPU {
         }
     }
 
-    pub fn load_game(&mut self, game: &mut dyn Read) {
-        game.read(&mut self.memory[0x200..]);
+    pub fn load_game(&mut self, game: &mut dyn Read) -> Result<(), std::io::Error> {
+        // TODO After reading take, try reading more - input buffer should be consumed
+        match game.take(MAX_GAME_SIZE as u64).read(&mut self.memory[APPLICATION_START_ADDRESS..]) {
+           Err(e) => Err(e),
+           Ok(_s) => Ok(()),
+        }
     }
 
-    pub fn load_game_from_file(&mut self, file_path: &Path) {
+    pub fn load_game_from_file(&mut self, file_path: &Path) -> Result<(), std::io::Error>  {
         let mut game_file = File::open(file_path).expect(
             format!(
                 "Couldn't open file {}",
@@ -57,7 +65,7 @@ impl CPU {
             .as_str(),
         );
 
-        self.load_game(&mut game_file);
+        self.load_game(&mut game_file)
     }
 }
 
@@ -91,8 +99,24 @@ mod tests {
         cpu.load_game_from_file(test_game_path);
 
         assert_eq!(
-            cpu.memory[0x200..(0x200 + file_contents.len())],
+            cpu.memory[APPLICATION_START_ADDRESS..(APPLICATION_START_ADDRESS + file_contents.len())],
             file_contents
         );
+    }
+
+    #[test]
+    fn test_load_game_too_long_errors() {
+        // implement readable trait
+        struct FakeReader;
+
+        impl std::io::Read for FakeReader {
+            fn read(&mut self, buf: &mut [u8]) -> Result<usize, std::io::Error> {
+                Ok(1)
+            }
+        }
+        let mut fake_reader = FakeReader{};
+
+        let mut cpu = CPU::initialize();
+        assert!(cpu.load_game(&mut fake_reader).is_err())
     }
 }
